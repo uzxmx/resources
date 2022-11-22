d41ac2b05d0c694a4b61949a80c55f94cb0dc66c
Mingxiang Xue <mingxiangxue@gmail.com>
2022-11-22 16:53:14 +0800

    Close webdriver session when done
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- examples/selenium/simple/main.py
+++ examples/selenium/simple/main.py
@@ -21,33 +21,37 @@ class Main:
         else:
             driver = webdriver.Chrome(options=options)
 
-        driver.get('https://www.baidu.com')
-        assert '百度' in driver.title
-        elem = driver.find_element(By.NAME, 'wd')
-        elem.clear()
-        elem.send_keys("百度")
-        elem.send_keys(Keys.RETURN)
+        try:
+            driver.get('https://www.baidu.com')
+            assert '百度' in driver.title
+            elem = driver.find_element(By.NAME, 'wd')
+            elem.clear()
+            elem.send_keys("百度")
+            elem.send_keys(Keys.RETURN)
 
-        print('Find elements by class name')
-        elements = WebDriverWait(driver, timeout=10).until(lambda d: d.find_elements(By.CLASS_NAME, 'c-title'))
-        print('Found %d elements' % len(elements))
-        assert len(elements) > 0
-        for e in elements:
-            assert e.tag_name == 'h3'
-            assert '百度' in e.text
+            print('Find elements by class name')
+            elements = WebDriverWait(driver, timeout=10).until(lambda d: d.find_elements(By.CLASS_NAME, 'c-title'))
+            print('Found %d elements' % len(elements))
+            assert len(elements) > 0
+            for e in elements:
+                assert e.tag_name == 'h3'
+                assert '百度' in e.text
 
-        print('Find elements by xpath')
-        elements = WebDriverWait(driver, timeout=10).until(lambda d: d.find_elements(By.XPATH, '//div[contains(@class, "result-op")]//h3[contains(@class, "c-title")]'))
-        print('Found %d elements' % len(elements))
-        assert len(elements) > 0
-        for e in elements:
-            assert e.tag_name == 'h3'
-            assert '百度' in e.text
+            print('Find elements by xpath')
+            elements = WebDriverWait(driver, timeout=10).until(lambda d: d.find_elements(By.XPATH, '//div[contains(@class, "result-op")]//h3[contains(@class, "c-title")]'))
+            print('Found %d elements' % len(elements))
+            assert len(elements) > 0
+            for e in elements:
+                assert e.tag_name == 'h3'
+                assert '百度' in e.text
 
-        if opts.wait_infinitely:
-            WebDriverWait(driver, timeout=3600).until(lambda d: False)
+            if opts.wait_infinitely:
+                WebDriverWait(driver, timeout=3600).until(lambda d: False)
 
-        driver.close()
+            driver.close()
+        finally:
+            # Close the session.
+            driver.quit()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
